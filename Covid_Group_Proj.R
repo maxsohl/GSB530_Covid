63f38e79fb8932396b0498fa928d6e16345c88e3
maxsohl <sohl@calpoly.edu>
2023-03-06 17:43:28 -0800

    Update Covid_Group_Proj.R

--- Covid_Group_Proj.R
+++ Covid_Group_Proj.R
@@ -1,7 +1,9 @@
 
 setwd("~/Desktop/Winter 2023/GSB 530/Group Project")
-
-
-Big_Data_Files
-
 df <- read_excel("Big_Data_Files.xlsx", sheet = "COVID_Testing")
+
+
+#Clean
+df$Age_60_And_Above <- ifelse(df$Age_60_And_Above == "Yes", 1, 0)
+df$Male <- ifelse(df$Sex == "male", 1, 0)
+df$Positive <- ifelse(df$Result == "positive", 1, 0)
